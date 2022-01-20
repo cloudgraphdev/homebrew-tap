6b9b522a0d15b03c9d3d48c6557af9b3e3b2efdd
tyler dunkel <tyler.dunkel@autocloud.dev>
2022-01-20 14:20:27 -0600

    CloudGraph v0.16.12

--- cg.rb
+++ cg.rb
@@ -2,37 +2,38 @@
 # Do not update this file directly;
 # Please update the template instead:
 # https://github.com/heroku/cli/blob/master/scripts/release/homebrew/templates/heroku.rb
-class Cg < Formula
-    desc "Query your cloud and SaaS data with GraphQL"
-    homepage "https://cloudgraph.dev"
-    url "https://cli-assets.cloudgraph.dev/__TAG_NAME__/cg-__TAG_NAME__-darwin-x64.tar.gz"
-    sha256 "__TARBALL_HASH__"
-  
-    def install
-      inreplace "bin/cg", /^CLIENT_HOME=/, "export CG_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-      libexec.install Dir["*"]
-      bin.install_symlink libexec/"bin/cg"
-  
-    #   bash_completion.install libexec/"node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/bash" => "heroku"
-    #   zsh_completion.install libexec/"node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/zsh/_heroku"
-    end
-  
-    # def caveats; <<~EOS
-    #   To use the Heroku CLI's autocomplete --
-    #     Via homebrew's shell completion:
-    #       1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-    #           NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-    #                 and called, either explicitly or via a framework like oh-my-zsh.
-    #       2) Then run
-    #         $ heroku autocomplete --refresh-cache
-    #     OR
-    #     Use our standalone setup:
-    #       1) Run and follow the install steps:
-    #         $ heroku autocomplete
-    # EOS
-    # end
-  
-    test do
-      system bin/"cg", "version"
-    end
+class Cloudgraph < Formula
+  desc "Query your cloud and SaaS data with GraphQL"
+  homepage "https://cloudgraph.dev"
+  url "https://cli-assets.cloudgraph.dev/cg-v0.16.12/cg-v0.16.12-6a2697e-darwin-x64.tar.gz"
+  sha256 "b313aa6427891cea4551b5c9c70f437a8d97d08533371e65a789ac56df8e54f2"
+  depends_on "cloudgraphdev/tap/cloudgraph-node" => "16.0.0"
+
+  def install
+    inreplace "bin/cg", /^CLIENT_HOME=/, "export CG_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/cg"
+
+  #   bash_completion.install libexec/"node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/bash" => "heroku"
+  #   zsh_completion.install libexec/"node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/zsh/_heroku"
   end
+
+  # def caveats; <<~EOS
+  #   To use the Heroku CLI's autocomplete --
+  #     Via homebrew's shell completion:
+  #       1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
+  #           NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
+  #                 and called, either explicitly or via a framework like oh-my-zsh.
+  #       2) Then run
+  #         $ heroku autocomplete --refresh-cache
+  #     OR
+  #     Use our standalone setup:
+  #       1) Run and follow the install steps:
+  #         $ heroku autocomplete
+  # EOS
+  # end
+
+  test do
+    system bin/"cg", "version"
+  end
+end
