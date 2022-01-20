e89ad09194c8935c1fdc6d9903663dacfba5eca8
tyler dunkel <tyler.dunkel@autocloud.dev>
2022-01-20 16:21:25 -0600

    CloudGraph v0.16.13

--- cg.rb
+++ cg.rb
@@ -6,7 +6,7 @@ class Cg < Formula
   desc "Query your cloud and SaaS data with GraphQL"
   homepage "https://cloudgraph.dev"
   url "https://cli-assets.cloudgraph.dev/cg-v0.16.13/cg-v0.16.13-darwin-x64.tar.gz"
-  sha256 "null"
+  sha256 "1a9cc7962388e434c423d20a6dda1eabdab4aa7baef1e520ebcea5928d2f87f3"
 
   def install
     inreplace "bin/cg", /^CLIENT_HOME=/, "export CG_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
